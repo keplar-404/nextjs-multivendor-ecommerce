fa7d87dbbb903df7aef0e109bd995803991101d3
keplar-404 <shehabhossen76.services@gmail.com>
2023-01-26 15:23:33 +0600

    order api bug fixes

--- server/models/order.js
+++ server/models/order.js
@@ -1,38 +1,39 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const orderSchema = mongoose.Schema({
-    name: {
-        type: String,
-      },
-      images: {
-        type: Array,
-        default: [],
-      },
-      price: {
-        type: Number,
-      },
-      quentity:{
-        type: Number
-      },
-      category: {
-        type: String,
-      },
-      shopname:{
-        type:String
-      },
-      delevertoadmin:{
-        type: Boolean,
-        default: false
-      },
-      delevered:{
-        type: Boolean,
-        default: false
-      },
-      customeremail: {
-        type: String
-      },
-      uid: String
-    });
+  name: {
+    type: String,
+  },
+  images: {
+    type: Array,
+    default: [],
+  },
+  price: {
+    type: Number,
+  },
+  quentity: {
+    type: Number,
+  },
+  category: {
+    type: String,
+  },
+  shopname: {
+    type: String,
+  },
+  delevertoadmin: {
+    type: Boolean,
+    default: false,
+  },
+  delevered: {
+    type: Boolean,
+    default: false,
+  },
+  customeremail: {
+    type: String,
+  },
+  uid: String,
+  order: String,
+});
 
-const OrderModel = mongoose.model('order', orderSchema)
-export default OrderModel
+const OrderModel = mongoose.model("order", orderSchema);
+export default OrderModel;
